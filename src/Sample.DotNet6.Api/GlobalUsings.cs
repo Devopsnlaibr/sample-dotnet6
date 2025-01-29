2acdb0cea65372e65b1d0097587e15a0341ce1d8
hazerak83 <60449131+hazerak83@users.noreply.github.com>
2025-01-29 20:28:45 +0100

    Update GlobalUsings.cs

--- src/Sample.DotNet6.Api/GlobalUsings.cs
+++ src/Sample.DotNet6.Api/GlobalUsings.cs
@@ -1,5 +1,7 @@
 ﻿global using Microsoft.AspNetCore.Mvc;
+
 global using Microsoft.OpenApi.Models;
+
 global using System.Security.Claims;
 
 global using static Sample.DotNet6.Api.Core.Extensions.WebExtensions;
